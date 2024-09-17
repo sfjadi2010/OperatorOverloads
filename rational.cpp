e88a010e50d4f81e311e399213dfc47a6c5feb6c
Salman Farsi <salman.farsi@3together.us>
2024-09-16 23:28:44 -0400

    added rational.cpp

---
+++ rational.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <format>
+#include <string>
+#include "rational.h"
+
+using namespace std;
+using std::string;
+
+Rational::Rational(int numerator = 0, int denominator = 1) : n(numerator), d(denominator) {}
